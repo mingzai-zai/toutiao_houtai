7700d0147997bfaae585c81a4cc10dbbfeda2873
mingzai-zai <mingzai_2020@163.com>
2020-01-11 23:03:43 +0800

    登录二次判断（请求）完成

---
+++ src/utils/myaxios.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+// 哪里需要默认就在哪里搞嘛真的是
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
+export default axios
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,37 @@
 <template>
   <div class="login">
     <div class="user_login">
-      <img src="../assets/avatar.jpg" alt="" class="pic"/>
+      <img src="../assets/avatar.jpg" alt="" class="pic" />
       <el-form
-        :model="ruleForm"
+        :model="user"
         :rules="rules"
-        ref="ruleForm"
+        ref="user"
         label-width="100px"
         class="demo-ruleForm"
       >
         <el-form-item label="用户名" prop="username">
           <el-input
-            v-model="ruleForm.username"
+            v-model="user.username"
             placeholder="请输入靓仔/靓女名字"
             clearable
             prefix-icon="el-icon-user-solid"
           ></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <!-- 可以prefix-icon 和suffix-icon 属性在 input 组件首部和尾部增加显示图标,也可以通过 slot 来放置图标。 -->
+          <!-- 可以prefix-icon 和suffix-icon 属性在 input 组件首部和尾部增加显示图标,也可以通过 slot 来放置图标。可以在按钮那个位置找 -->
           <el-input
             type="password"
-            v-model="ruleForm.password"
-            autocomplete="off"
+            v-model="user.password"
             placeholder="请输入昂仔/昂女密码"
             clearable
             prefix-icon="el-icon-key"
           ></el-input>
         </el-form-item>
-        <el-form-item >
-          <el-button type="danger" @click="submitForm('ruleForm')" class="btn"
+        <el-form-item>
+          <el-button type="danger" @click="submitForm" class="btn"
             >立即登录</el-button
           >
-          <el-button @click="resetForm('ruleForm')" class="btn">重置</el-button>
+          <el-button @click="resetForm" class="btn">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -40,13 +39,13 @@
 </template>
 
 <script>
+import { login } from '@/apis/user.js';
 export default {
   data() {
     return {
-      ruleForm: {
-        username: '',
-        type: ['primary'],
-        password: ''
+      user: {
+        username: '10086',
+        password: '123456'
       },
       rules: {
         username: [
@@ -67,19 +66,28 @@ export default {
     };
   },
   methods: {
-    submitForm(formName) {
+    submitForm() {
       // validate任一表单项被校验后触发
-      this.$refs[formName].validate(result => {
-        console.log(result);
-        // if (valid) {
-        //   alert('submit!');
-        // } else {
-        //   console.log('error submit!!');
-        //   return false;
-        // }
+      this.$refs.user.validate(async valid => {
+        console.log(valid);
+        // console.log(this.$refs);
+        if (valid) {
+          //   console.log(this.user)
+          //   alert('submit!');
+          let res = await login(this.user);
+          // console.log(res);
+          if (res.data.message === '用户不存在') {
+            this.$message.warning(res.data.message)
+          } else {
+            console.log(111)
+          }
+        } else {
+          console.log('啊啊啊出错了');
+          return false;
+        }
       });
     },
-    resetForm(formName) {
+    resetForm() {
       //   this.$refs[formName].resetFields();
       // resetFields()重置
       //   console.log(formName)---ruleForm
@@ -101,18 +109,20 @@ export default {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-55%,-55%);
+    transform: translate(-55%, -55%);
     width: 450px;
     margin: 0 auto;
-    .pic{
-        position: absolute;
-        left: 45%;
-        top: -75%;
-        border-radius: 50%;
-        border: 5px dotted #000;
+    .pic {
+      width: 120px;
+      height: 120px;
+      position: absolute;
+      left: 45%;
+      top: -75%;
+      border-radius: 50%;
+      border: 5px dotted #000;
     }
-    .btn{
-        margin-left: 60px;
+    .btn {
+      margin-left: 60px;
     }
   }
 }
